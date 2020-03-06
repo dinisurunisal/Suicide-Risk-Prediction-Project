140a779b8abfe1e64ab17f5287aa779aa6008521
Malsha Piumini <malsha.2018443@iit.ac.lk>
2020-03-07 01:35:58 +0530

    have to enter doctor API's

---
+++ Backend/First_Project/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class Person {
+    private String NIC;
+
+    public Person(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+}
