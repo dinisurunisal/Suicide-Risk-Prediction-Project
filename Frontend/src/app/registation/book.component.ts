1f6a68a032144129c8339ccf0b13e523060f4453
Nethmimithara <50904142+Nethmimithara@users.noreply.github.com>
2020-03-10 14:29:08 +0530

    Update book.component.ts
    Initial state

--- Frontend/src/app/registation/book.component.ts
+++ Frontend/src/app/registation/book.component.ts
@@ -1,51 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Router } from '@angular/router';
+{ path: 'register', loadChildren: './auth/register/register.module#RegisterPageModule' }, 
 
-@Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
-})
-export class BookComponent implements OnInit {
-  nameToReg = new FormControl();
-  passwordToReg = new FormControl();
-  emailToReg = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+import { Component, OnInit } from '@angular/core'; 
+import { Router } from "@angular/router";
+import { AuthService } from '../auth.service';
 
-//register ek seccussfull nan data tika pass krnn 
-  constructor(private url:HttpClient, public router:Router) { }
-
-  ngOnInit() {
-  }
-
-  register(){
-    var regName =this.nameToReg.value;
-    var regEmail= this.emailToReg.value;
-    var regPassword = this.passwordToReg.value;
-
-    let regParameter = new HttpParams({
-      fromObject:{
-        "name":regName,
-        "mail":regEmail,
-        "password":regPassword
-
-      }
-    });
-    var request ='http://ec2-18-221-114-73.us-east-2.compute.amazonaws.com:8080/api/reg?name&mail&password'
-
-    this.url.post<any>(request,regParameter).subscribe(
-      data =>{
-        alert("You're Succesfully Registered")
-        this.router.navigate(
-          ["/login"]
-        );
-      }
-    )
-  }
-
-}
-
+@Component({ selector: 'app-register', templateUrl: './register.page.html', styleUrls: ['./register.page.scss'], }) export class RegisterPage implements OnInit { constructor(private authService: AuthService, private router: Router) { } ngOnInit() { } }
