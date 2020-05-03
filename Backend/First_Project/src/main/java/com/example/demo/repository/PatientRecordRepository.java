0bef250aaf58575b6170ee44be36d283bf7efa5c
MalshaPiumini <malsha.2018443@iit.ac.lk>
2020-05-04 00:05:40 +0530

    delete spring first project

--- Backend/First_Project/src/main/java/com/example/demo/repository/PatientRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Doctor;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PatientRecordRepository  extends MongoRepository<PatientRecordRepository,String> {
-    public Doctor findByNIC(String NIC);
-}
